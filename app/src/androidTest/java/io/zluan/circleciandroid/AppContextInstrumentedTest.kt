dbb7341e1cf7a5f0d4df9683de11369524cc5de2
Zhaonian Luan <lzn0830@gmail.com>
2019-09-12 18:16:06 -0700

    set up instrumentation tests

--- app/src/androidTest/java/io/zluan/circleciandroid/AppContextInstrumentedTest.kt
+++ app/src/androidTest/java/io/zluan/circleciandroid/AppContextInstrumentedTest.kt
@@ -4,8 +4,7 @@ import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import org.junit.Test
 import org.junit.runner.RunWith
-
-import org.junit.Assert.*
+import org.junit.Assert.assertEquals
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -13,7 +12,7 @@ import org.junit.Assert.*
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 @RunWith(AndroidJUnit4::class)
-class ExampleInstrumentedTest {
+class AppContextInstrumentedTest {
 
     @Test fun useAppContext() {
         // Context of the app under test.
