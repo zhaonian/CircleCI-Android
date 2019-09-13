bf727bbb95647435c06f1d8f50fc21ffee31ccbe
Zhaonian Luan <lzn0830@gmail.com>
2019-09-12 18:12:03 -0700

    fix test dependency issue

--- app/src/androidTest/java/io/zluan/circleciandroid/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/io/zluan/circleciandroid/ExampleInstrumentedTest.kt
@@ -2,7 +2,6 @@ package io.zluan.circleciandroid
 
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
-
 import org.junit.Test
 import org.junit.runner.RunWith
 
@@ -15,8 +14,8 @@ import org.junit.Assert.*
  */
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
-    @Test
-    fun useAppContext() {
+
+    @Test fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
         assertEquals("io.zluan.circleciandroid", appContext.packageName)
