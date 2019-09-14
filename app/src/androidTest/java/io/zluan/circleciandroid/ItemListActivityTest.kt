7ac67572698532a16c170a53a861218c4a77d73f
Zhaonian Luan <lzn0830@gmail.com>
2019-09-13 19:16:18 -0700

    add basic espresso test on toolbar

--- app/src/androidTest/java/io/zluan/circleciandroid/ItemListActivityTest.kt
+++ app/src/androidTest/java/io/zluan/circleciandroid/ItemListActivityTest.kt
@@ -1,3 +1,26 @@
 package io.zluan.circleciandroid
 
-class ItemListActivityTest
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.rule.ActivityTestRule
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+/**
+ * The Rule will make sure to launch the MainActivity directly. This means that when testing an
+ * n-layer activity, you don’t need to do all the steps to start it. Simply define that in the rule
+ * and you are ready to go.
+ */
+@RunWith(AndroidJUnit4::class)
+class ItemListActivityTest {
+
+    @get:Rule val activityTestRule = ActivityTestRule<ItemListActivity>(ItemListActivity::class.java)
+
+    @Test fun toolBar_showAppTitle() {
+        onView(withId(R.id.toolbar)).check(matches(isDisplayed()))
+        onView(withText(R.string.app_name)).check(matches(withParent(withId(R.id.toolbar))))
+    }
+}
